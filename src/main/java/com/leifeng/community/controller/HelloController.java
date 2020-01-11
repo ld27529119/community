e468f97a5f62facf3aed183a0abc9112583cc018
ld27529119 <27529119@qq.com>
2020-01-11 10:20:59 +0800

    init repo

---
+++ src/main/java/com/leifeng/community/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.leifeng.community.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+/**
+ * Created by ThinkPad on 2020/1/2.
+ */
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
